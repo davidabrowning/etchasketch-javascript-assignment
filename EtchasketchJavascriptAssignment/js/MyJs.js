f49c79f8dbe520780f24fe453f7472d9a68f4d0e
David Browning <david.a.browning@gmail.com>
2025-05-07 19:38:45 +0200

    Get first javascript class working.

--- EtchasketchJavascriptAssignment/js/MyJs.js
+++ EtchasketchJavascriptAssignment/js/MyJs.js
@@ -2,3 +2,19 @@ function testJs() {
     let htmlResults = document.getElementsByClassName("section-title");
     htmlResults[0].classList.add("blue");
 }
+
+function createAndGreet() {
+    john = new Person("John");
+    john.greet("Jane");
+}
+
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    greet(otherName) {
+        const paragraphs = document.getElementsByTagName("p");
+        const greeting = document.createTextNode("Hello, " + otherName + " -" + this.name);
+        paragraphs[0].appendChild(greeting);
+    }
+}
