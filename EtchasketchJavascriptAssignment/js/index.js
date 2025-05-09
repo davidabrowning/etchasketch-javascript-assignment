e1661ca8c5d4cfc1726b77e0c72eff1cdd8c1936
David Browning <david.a.browning@gmail.com>
2025-05-09 15:53:30 +0200

    Allow WASD support.

--- EtchasketchJavascriptAssignment/js/index.js
+++ EtchasketchJavascriptAssignment/js/index.js
@@ -6,15 +6,23 @@ drawingBoard.initialize();
 document.onkeydown = function(e) {
     switch (e.key) {
         case "ArrowLeft":
+        case "a":
+        case "A":
             drawingBoard.moveLeft();
             break;
         case "ArrowUp":
+        case "w":
+        case "W":
             drawingBoard.moveUp();
             break;
         case "ArrowRight":
+        case "d":
+        case "D":
             drawingBoard.moveRight();
             break;
         case "ArrowDown":
+        case "s":
+        case "S":
             drawingBoard.moveDown();
             break;
     }
