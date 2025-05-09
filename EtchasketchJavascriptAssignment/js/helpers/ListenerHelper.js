19f1745a870c834a27728628ea98cbc856f53104
David Browning <david.a.browning@gmail.com>
2025-05-09 15:57:13 +0200

    Move listener setup to new ListenerHelper class.

---
+++ EtchasketchJavascriptAssignment/js/helpers/ListenerHelper.js
@@ -0,0 +1,31 @@
+export class ListenerHelper{
+    static addListeners(drawingBoard) {
+        ListenerHelper.addStylusListeners(drawingBoard);
+    }
+    static addStylusListeners(drawingBoard) {
+        document.onkeydown = function(e) {
+            switch (e.key) {
+                case "ArrowLeft":
+                case "a":
+                case "A":
+                    drawingBoard.moveLeft();
+                    break;
+                case "ArrowUp":
+                case "w":
+                case "W":
+                    drawingBoard.moveUp();
+                    break;
+                case "ArrowRight":
+                case "d":
+                case "D":
+                    drawingBoard.moveRight();
+                    break;
+                case "ArrowDown":
+                case "s":
+                case "S":
+                    drawingBoard.moveDown();
+                    break;
+            }
+        }
+    }
+}
--- EtchasketchJavascriptAssignment/js/index.js
+++ EtchasketchJavascriptAssignment/js/index.js
@@ -1,29 +1,6 @@
 import { DrawingBoard } from "./models/DrawingBoard.js";
+import { ListenerHelper } from "./helpers/ListenerHelper.js";
 
 let drawingBoard = new DrawingBoard();
 drawingBoard.initialize();
-
-document.onkeydown = function(e) {
-    switch (e.key) {
-        case "ArrowLeft":
-        case "a":
-        case "A":
-            drawingBoard.moveLeft();
-            break;
-        case "ArrowUp":
-        case "w":
-        case "W":
-            drawingBoard.moveUp();
-            break;
-        case "ArrowRight":
-        case "d":
-        case "D":
-            drawingBoard.moveRight();
-            break;
-        case "ArrowDown":
-        case "s":
-        case "S":
-            drawingBoard.moveDown();
-            break;
-    }
-}
+ListenerHelper.addListeners(drawingBoard);
