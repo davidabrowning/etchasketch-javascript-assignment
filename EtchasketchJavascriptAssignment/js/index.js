d78e0b1f1a77f010e7f131c2f1659004cef59e10
David Browning <david.a.browning@gmail.com>
2025-05-09 15:51:00 +0200

    Replace deprecated keyCode with e.key.

--- EtchasketchJavascriptAssignment/js/index.js
+++ EtchasketchJavascriptAssignment/js/index.js
@@ -4,12 +4,18 @@ let drawingBoard = new DrawingBoard();
 drawingBoard.initialize();
 
 document.onkeydown = function(e) {
-    if (e.keyCode == 37) // left arrow
-        drawingBoard.moveLeft();
-    if (e.keyCode == 38) // up arrow
-        drawingBoard.moveUp();
-    if (e.keyCode == 39) // right arrow
-        drawingBoard.moveRight();
-    if (e.keyCode == 40) // down arrow
-        drawingBoard.moveDown();
+    switch (e.key) {
+        case "ArrowLeft":
+            drawingBoard.moveLeft();
+            break;
+        case "ArrowUp":
+            drawingBoard.moveUp();
+            break;
+        case "ArrowRight":
+            drawingBoard.moveRight();
+            break;
+        case "ArrowDown":
+            drawingBoard.moveDown();
+            break;
+    }
 }
